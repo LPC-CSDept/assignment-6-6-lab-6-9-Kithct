41b080e24eb5b89171546ace6f13134b87e34a27
Zelin Chen <6692czl@gmail.com>
2022-10-09 21:30:43 -0700

    A66(1)

--- A66.hpp
+++ A66.hpp
@@ -5,3 +5,36 @@
 using namespace std;
 
 // complete the function maxmin( )
+int getinput(int &,int &,int &)
+{
+    int n1,n2,n3;
+    cin>>n1>>n2>>n3;
+}
+int maxmin(int,int,int,int &,int &)
+{
+    int n1,n2,n3;
+    int max,min;
+    max=min=n1;
+    if(n1>n2)
+    {
+        min=n2;
+        if(n2>n3)
+            min=n3;
+        else
+        {
+            if(n1<n3)
+                max=n3;
+        }
+    }
+    else
+    {
+        max=n2;
+        if(n3>n2)
+            max=n3;
+        else
+        {
+            if(n3>n1)
+                min=n1;
+        }
+    }
+}
